8983d581e1f8f7774e122b2f0f8f61517a994843
Silke <silke@slxh.eu>
2016-06-05 23:52:41 +0200

    Quick shell implementation

---
+++ README.md
@@ -0,0 +1,27 @@
+# pdns_api.sh
+A simple DNS hook that lets [`letsencrypt.sh`][le.sh] talk to the PowerDNS API.
+
+# Usage
+Add the settings for your PowerDNS API to
+`/etc/letsencrypt.sh/config`, `/usr/local/etc/letsencrypt.sh/config`
+or a `config` file next to `pdns_api.sh`:
+
+```sh
+HOST=ns0.example.com
+PORT=8081
+KEY=secret           # API key
+VERSION=1            # API version, 0 for anything under PowerDNS 4.
+SERVER=localhost     # Server for the API to use, usually `localhost`
+WAIT=300             # Optional, for when slaves are slow
+```
+
+Configure it as a DNS hook, by adding the following to your `letsencrypt.sh` config:
+
+```sh
+CHALLENGETYPE="dns-01"
+HOOK="./pdns_api.sh"
+```
+
+`HOOK_CHAIN` is not supported at this time.
+
+[le.sh]: https://github.com/lukas2511/letsencrypt.sh
---
+++ pdns_api.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+
+# Copyright 2016 - Silke Hofstra
+#
+# Licensed under the EUPL, Version 1.1 or -- as soon they will be approved by
+# the European Commission -- subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+#
+
+# Local directory
+DIR="$(dirname "${0}")"
+
+# Check for config in various locations
+# From letsencrypt.sh
+if [[ -z "${CONFIG:-}" ]]; then
+  for check_config in "/etc/letsencrypt.sh" "/usr/local/etc/letsencrypt.sh" "${PWD}" "${DIR}"; do
+    if [[ -f "${check_config}/config" ]]; then
+      CONFIG="${check_config}/config"
+      break
+    fi
+  done
+fi
+
+# Load configuration
+. "$CONFIG"
+
+# Domain and token from arguments
+domain="${2}"
+token="${4}"
+
+# Create the zone name from the arguments
+IFS='.' read -a domain_array <<< "$domain"
+zone="${domain_array[*]: -2:1}.${domain_array[*]: -1:1}"
+
+# Record name
+name="_acme-challenge.$domain"
+
+# URL to post to
+url="/servers/$SERVER/zones/$zone"
+
+# Header with the api key
+headers="X-API-Key: $KEY"
+
+# Some version incompatibilities
+if [[ $VERSION -ge 1 ]]; then
+  name="$name."
+  url="/api/v${VERSION}${url}"
+else
+  extra_data=",\"name\": \"${name}\", \"type\": \"TXT\", \"ttl\": 1"
+fi
+
+# Add the host and port to the url
+url="http://${HOST}:${PORT}${url}"
+
+## Functions
+# API request
+request () {
+  # Do the request
+  res=$(curl -sS --request PATCH --header "$headers" --data "$data" "$url")
+
+  # Debug output
+  if [[ "$DEBUG" ]]; then
+    echo "Data: $data"
+    echo "Response: $res"
+  fi
+
+  # Abort on failed request
+  if [[ "$?" -gt 0 ]]; then
+    echo "Request to API failed."
+    exit 1
+  elif [[ $res = *"error"* ]]; then
+    echo "API error: $res"
+    exit 1
+  fi
+}
+
+## Actions
+
+# Deploy a token
+if [[ "$1" = "deploy_challenge" ]]; then
+  # Create the JSON string
+  data='{
+    "rrsets": [{
+      "name": "'${name}'",
+      "type": "TXT",
+      "ttl": 1,
+      "records": [{
+        "content": "\"'${token}'\"",
+        "disabled": false,
+        "set-ptr": false
+        '${extra_data}'
+      }],
+      "changetype": "REPLACE"
+    }]
+  }'
+
+  # Do the request
+  request
+fi
+
+# Remove a token
+if [[ "$1" = "clean_challenge" ]]; then
+  # Create the JSON string
+  data='{"rrsets":[{"name":"'${name}'","type":"TXT","changetype":"DELETE"}]}'
+
+  # Do the request
+  request
+fi
+
+# Other actions are not implemented but will not cause an error
+
+exit 0
