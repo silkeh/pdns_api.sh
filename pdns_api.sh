367b3401c90d2337cea01f558b1caf4037d358ac
Grewe, Sven <sven.grewe@akquinet.de>
2020-05-27 11:23:52 +0200

    [ADD] deploy_cert hook handler
    Wrote a handler for the deploy_cert hook which checks the
    PDNS_DEPLOY_CERT_HOOK variable from config and executes its content if
    non empty

--- pdns_api.sh
+++ pdns_api.sh
@@ -371,6 +371,12 @@ exit_hook() {
   fi
 }
 
+deploy_cert() {
+  if [[ ! -z "${PDNS_DEPLOY_CERT_HOOK:-}" ]]; then
+    ${PDNS_DEPLOY_CERT_HOOK}
+  fi
+}
+
 main() {
   # Set hook
   hook="$1"
@@ -379,7 +385,7 @@ main() {
   debug "Hook: ${hook}"
 
   # Ignore unknown hooks
-  if [[ ! "${hook}" =~ ^(deploy_challenge|clean_challenge|soa_edit|exit_hook)$ ]]; then
+  if [[ ! "${hook}" =~ ^(deploy_challenge|clean_challenge|soa_edit|exit_hook|deploy_cert)$ ]]; then
     exit 0
   fi
 
@@ -400,6 +406,12 @@ main() {
   if [[ "${hook}" = "exit_hook" ]]; then
     shift
     exit_hook "$@"
+    exit 0
+  fi
+
+  # Interface for deploy_cert
+  if [[ "${hook}" = "deploy_cert" ]]; then
+    deploy_cert "$@"
     exit 0
   fi
 
