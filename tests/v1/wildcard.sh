652c18ab70d4995b809483b21df5bc4107a2542b
Silke Hofstra <silke@slxh.eu>
2020-09-16 12:24:04 +0200

    Add test cases for wildcard records without a *
    The current test cases assume a wild card was given on the command line directly.
    However, current versions of dehydrated provide them without.
    Add two test cases for this.

--- tests/v1/wildcard.sh
+++ tests/v1/wildcard.sh
@@ -2,6 +2,32 @@
 
 _TEST "V1 Wildcard"
 _SUBTEST "Deploy"
+_RUN 'deploy_challenge example.com _ t1 example.com _ t2' \
+'PATCH http://hostname:8000/api/v1/servers/servername/zones/example.com.' \
+'{
+  "rrsets": [
+    {
+      "name": "_acme-challenge.example.com.",
+      "type": "TXT",
+      "ttl": 1,
+      "records": [
+        {
+          "content": "\"t2\"",
+          "disabled": false,
+          "set-ptr": false
+        },
+        {
+          "content": "\"t1\"",
+          "disabled": false,
+          "set-ptr": false
+        }
+      ],
+      "changetype": "REPLACE"
+    }
+  ]
+}'
+
+_SUBTEST "Deploy *"
 _RUN 'deploy_challenge example.com _ t1 *.example.com _ t2' \
 'PATCH http://hostname:8000/api/v1/servers/servername/zones/example.com.' \
 '{
@@ -28,6 +54,19 @@ _RUN 'deploy_challenge example.com _ t1 *.example.com _ t2' \
 }'
 
 _SUBTEST "Clean"
+_RUN 'clean_challenge example.com _ t1 example.com _ t2' \
+'PATCH http://hostname:8000/api/v1/servers/servername/zones/example.com.' \
+'{
+  "rrsets": [
+    {
+      "name": "_acme-challenge.example.com.",
+      "type": "TXT",
+      "changetype": "DELETE"
+    }
+  ]
+}'
+
+_SUBTEST "Clean *"
 _RUN 'clean_challenge example.com _ t1 *.example.com _ t2' \
 'PATCH http://hostname:8000/api/v1/servers/servername/zones/example.com.' \
 '{
